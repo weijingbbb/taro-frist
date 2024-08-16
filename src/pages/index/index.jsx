0ab7003d432333d83300b999955a3ebceb072daa
weijingbbb <1047129443@qq.com>
2024-08-17 01:27:54 +0800

    配置husky和lint-staged，在提交github前进行eslint校验

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,6 +1,8 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import './index.scss'
+import { Text, View } from '@tarojs/components';
+import { useLoad } from '@tarojs/taro';
+import './index.scss';
+
+const a = 'bbb';
 
 export default function Index () {
   useLoad(() => {
