63bee3c7998ab892b2a939ead07d7cd45492dac7
weijingbbb <1047129443@qq.com>
2024-08-17 22:13:10 +0800

    mysql创建表成功

--- server/models/ads.js
+++ server/models/ads.js
@@ -2,7 +2,37 @@ const express = require('express')
 const router = express.Router()
 const sqlQuery = require("../mysql")
 
+
+const imgLists = [
+  "https://images3.c-ctrip.com/ztrip/flightbanner/student_authentication/%25E6%259C%25BA%25E7%25A5%25A8%25E9%25A6%2596%25E9%25A1%25B5a3x.png",
+  "https://images3.c-ctrip.com/ztrip/flightbanner/yaoxinyouli/app%25E9%25A6%2596%25E9%25A1%25B5.png"
+]
+
+const createTable = async () => {
+  console.log('创建表---广告');
+  try {
+    // 创建表
+    const createTablesql = `
+  create table if not exists ads (
+    id int auto_increment,
+    imgUrl char(255) not null,
+    primary key (id)
+  ) engine=innodb;
+  `;
+    await sqlQuery(createTablesql)
+    for (let i = 0; i < imgLists.length; i++) {
+      const insertSgl = `insert into ads(id, imgUrl) values(null, '${imgLists[i]}')`;
+      await sqlQuery(insertSgl)
+    }
+  }catch(err) {
+    console.log('出错了。。。',err);
+  }
+}
+
+createTable()
+
 router.get("/advertising", async (req, res) => {
+  console.log('访问了广告路由---advertising');
   const strSql = `select * from ads`;
   try {
     const result = await sqlQuery(strSql);
--- server/models/airportList.js
+++ server/models/airportList.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const sqlQuery = require("../mysql");
-const request = require("request")
+// const request = require("request")
 
 // demo
 // 创建机场城市列表mysql
